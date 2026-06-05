16293e8c17f420ffc4c9752b0bd8354dc9852a5a
Jacek <0288007@schoolsnet.act.edu.au>
2026-06-05 11:05:16 +1000

    Delete momos-crusher directory

--- momos-crusher/scripts/c3main.js
+++
@@ -1,5 +0,0 @@
-import "./../box2d.wasm.js";
-import "./c3runtime.js";
-import "./objRefTable.js";
-import "./project/lz-string.min.js";
-import "./project/javaScriptInEvents.js";
--- momos-crusher/wakerpr.js
+++
@@ -1,1 +0,0 @@
-importScripts('https://cdn.jsdelivr.net/gh/bubbls/UGS-Assets@main/momos-crusher/waker.js');
